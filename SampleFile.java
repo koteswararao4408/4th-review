414f071ebb02037545fc95ee3cb39b4c04608eee
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-26 20:29:47 +0530

    Add:Review program

--- SampleFile.java
+++ SampleFile.java
@@ -2,13 +2,11 @@ public class SampleFile
 {
 	public static void main(String[] args)
 	{
+		int n=0;
 		for(int i=1;i<=5;i++)
 		{
-			for(int j=1;j<=i;j++)
-			{
-			System.out.println(j+"");
-			}
-			System.out.println();
+			n=n*10+i;
+			System.out.println(n+"");
 		}
 
 	}
